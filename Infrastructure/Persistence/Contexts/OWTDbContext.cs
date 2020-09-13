c3515829f45c339331e183b69768cfc27d654168
deeplygends <peeren.pro@gmail.com>
2020-09-13 10:30:12 +0200

    [Infrastructure][Configuration] Add DbContext + Abstract layer

---
+++ Infrastructure/Persistence/Contexts/OWTDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Contexts
+{
+    public class OWTDbContext : DbContext, IApplicationDbContext
+    {
+        public OWTDbContext(DbContextOptions<OWTDbContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #region Entities
+        public DbSet<Contact> Contacts { get; set; }
+        #endregion
+    }
+}
---
+++ Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿
+using Infrastructure.Persistence.Contexts;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<OWTDbContext>(options =>
+                    options.UseInMemoryDatabase("ApplicationDb"));
+            }
+            else
+            {
+                services.AddDbContext<OWTDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        b => b.MigrationsAssembly("API")));
+            }
+        }
+    }
+}
