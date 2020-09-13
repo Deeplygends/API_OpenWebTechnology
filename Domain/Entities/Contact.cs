cbb77f850dfd7988d67058f94a5b0aeff5bf6e6e
deeplygends <peeren.pro@gmail.com>
2020-09-13 09:30:14 +0200

    [Init] init master architecture

---
+++ Domain/Entities/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Contact : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => LastName + " " + FirstName;
+
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
